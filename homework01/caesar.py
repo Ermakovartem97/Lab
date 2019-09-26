a3f0f61764bdd108e0f715a7647171923ce9d3a9
ermakovartem97 <ermakovartem97@mail.ru>
2019-09-27 01:49:35 +0300

    Добавлен шаблон для шифра Цезаря

---
+++ homework01/caesar.py
@@ -0,0 +1,36 @@
+def encrypt_caesar(plaintext):
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    ciphertext = ""
+    for i in range(len(plaintext)):
+        ciphertext = ciphertext + chr(ord(plaintext[i]) + 3)
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext):
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+    return plaintext
+a = encrypt_caesar("PYTHON")
+print(a)
