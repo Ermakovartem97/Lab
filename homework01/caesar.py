3fbb2585ba8fe6b33709360869c9b548fe2511be
ermakovartem97 <ermakovartem97@mail.ru>
2019-09-27 01:53:51 +0300

    Реализован шифр Цезаря

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,14 +2,6 @@ def encrypt_caesar(plaintext):
     """
     Encrypts plaintext using a Caesar cipher.
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
     """
     ciphertext = ""
     for i in range(len(plaintext)):
@@ -19,14 +11,6 @@ def decrypt_caesar(ciphertext):
     """
     Decrypts a ciphertext using a Caesar cipher.
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
     """
     plaintext = ""
     for i in range(len(ciphertext)):
